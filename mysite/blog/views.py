f0af23e28c0ec45ec6cba226454d0ca52ac7ec45
Lucas Araujo <lucasaraujoo44.la@gmail.com>
2023-04-11 23:00:42 -0300

    Feat: adicionamos recursos de html ao codigo

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -4,4 +4,15 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Funcionando")
+    context = {}
+    return render(request, 'blogtemplate.html', context)
+
+def detail(request, question_id):
+    return HttpResponse("Essa é a página da questão %s." % question_id)
+
+def results(request, question_id):
+    response = ("Essa é a página da  resultados %s.")
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("Essa é uma página de votação da questão %s. " %question_id)
